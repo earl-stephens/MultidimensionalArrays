e6ee491a3f14c9bb7b67e87acfc05522a1a62ce0
earl-stephens <stephensearl@hotmail.com>
2021-12-22 20:24:51 -0700

    Show multidimensional arrays of strings

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,26 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// multidimensional array is an array of arrays
 
+		String[][] texts = { 
+				{ "one", "two", "three" }, 
+				{ "four", "five", "six" }, 
+				{ "seven", "eight", "nine" } };
+		
+		// have to use nested loops to iterate through the arrays
+
+		for(int i = 0; i < texts.length; i++) {
+			//this outputs three array object references
+			//System.out.println(texts[i]);
+			
+			String[] subArray = texts[i];
+			
+			for(int j = 0; j < subArray.length; j++) {
+				System.out.printf("%s\t", subArray[j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
